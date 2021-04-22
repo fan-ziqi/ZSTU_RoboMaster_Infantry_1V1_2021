62eedc6eeadc5410a4574fa65ef1bbf666068a4b
范子琦 <62320343+fanziqi614@users.noreply.github.com>
2021-04-22 20:38:52 +0800

    Add files via upload

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'standard_tpye_c' 
+ * Target:  'standard_tpye_c' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/RTE/_standard_tpye_c/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'standard_tpye_c' 
+ * Target:  'standard_tpye_c' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
